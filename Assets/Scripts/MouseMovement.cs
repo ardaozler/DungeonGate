0d2583b00800f81177d1358350d31c9227baa6fa
Arda Özler <ardaozler@yahoo.com>
2024-04-07 15:07:23 +0200

    feat: player controller improved, probuilder added

--- Assets/Scripts/MouseMovement.cs
+++ Assets/Scripts/MouseMovement.cs
@@ -2,26 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement_Script : MonoBehaviour
+public class MouseMovement : MonoBehaviour
 {
+    [SerializeField] private Transform _body;
+
     public float mouseSensitivity = 100f;
     float xRotation = 0f;
     float yRotation = 0f;
 
     public float topClamp = -90f;
     public float bottomClamp = 90f;
-    
+
     void Start()
     {
-       //Cursor lock
+        //Cursor lock
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //mouse inputlarý
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime ;
+        //mouse inputs
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
         //Rotation through x axis
@@ -29,13 +31,12 @@ public class Movement_Script : MonoBehaviour
 
         //Rotation kitleme
         xRotation = Mathf.Clamp(xRotation, topClamp, bottomClamp);
-        
+
         //Rotation through y axis
         yRotation += mouseX;
 
         //Apply Rotations to transform
-        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
-
-
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        _body.localRotation = Quaternion.Euler(0f, yRotation, 0f);
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,81 +1,178 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private CharacterController controller;
-    
-    public float speed = 12f;
-    public float gravity = -9.81f * 2;
+    private CharacterController _controller;
+
+    [Header("Movement")] public float speed = 12f;
+
+    [Header("Jump")] public float gravity = -9.81f * 2;
     public float jumpHeight = 3f;
+    public float jumpTopDelay = 0.1f;
+    public float coyoteTime = .1f;
+    public float jumpFallGravModifier = 1.5f;
 
 
-    public Transform groundcheck;
+    [Header("Ground Check")] public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
 
+    //Timers
+    private float _jumpTopTimer;
+    private float _coyoteTimer;
 
 
-    Vector3 velocity;
+    private Vector2 _downwardVelocity;
+    private Vector2 _lastDownwardVelocity = new Vector2(0f, 0f);
 
-    bool isGrounded;
-    bool isMoving;
+    private bool _isGrounded;
+    private bool _isMoving;
 
-    private Vector3 lastPosition = new Vector3(0f, 0f, 0f);
-    
-    
-    
-    
-    void Start()
+    private Vector3 _movementInput;
+
+    private Vector3 _lastPosition = new Vector3(0f, 0f, 0f);
+
+    private bool _hasJumped;
+
+    void Awake()
     {
-        controller = GetComponent<CharacterController>();
+        _controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Ground check
-
-        isGrounded = Physics.CheckSphere(groundcheck.position, groundDistance, groundMask);
-        //Reseting Velocity
-        if (isGrounded && velocity.y < 0)
-        {
-            velocity.y = -2f;
-
-        }
         //Getting inputs
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        _movementInput.x = Input.GetAxis("Horizontal");
+        _movementInput.z = Input.GetAxis("Vertical");
 
+        HandleJump();
+        HandleTimers();
+        HandleMovement();
 
+        // Ground check
+        _isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (_isGrounded && !_hasJumped)
+        {
+            _coyoteTimer = coyoteTime;
+        }
+    }
+
+    private void HandleMovement()
+    {
         //Creating moving vector
-        Vector3 move = transform.right * x + transform.forward * z;
+        Vector3 move = transform.right * _movementInput.x + transform.forward * _movementInput.z;
 
         //Character movement
-        controller.Move(move * speed * Time.deltaTime);
+        _controller.Move(move * (speed * Time.deltaTime));
 
-        //No infinite jump
-        if (Input.GetButtonDown("Jump") && isGrounded)
+
+        if (_lastPosition != gameObject.transform.position && _isGrounded)
         {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-
-        }
-        //Falling Down
-        velocity.y += gravity * Time.deltaTime;
-
-        // Jumping
-        controller.Move(velocity * Time.deltaTime);
-        if(lastPosition!=gameObject.transform.position&&isGrounded == true)
-        {
-            isMoving = true;
-
+            _isMoving = true;
         }
         else
         {
-            isMoving = false;
-
+            _isMoving = false;
         }
-        lastPosition=gameObject.transform.position; 
+
+        _lastPosition = gameObject.transform.position;
+    }
+
+    private void HandleTimers()
+    {
+        if (_jumpTopTimer > 0)
+        {
+            _jumpTopTimer -= Time.deltaTime;
+        }
+        else
+        {
+            _jumpTopTimer = 0;
+        }
+
+        if (_coyoteTimer > 0)
+        {
+            _coyoteTimer -= Time.deltaTime;
+        }
+        else
+        {
+            _coyoteTimer = 0;
+        }
+    }
+
+    private void HandleJump()
+    {
+        //if the coyote timer is higher than 0 that means that we are grounded or we left the ground very recently 
+        if (Input.GetButtonDown("Jump") && _coyoteTimer > 0)
+        {
+            _hasJumped = true;
+            _coyoteTimer = 0;
+            _downwardVelocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+
+        //not on the ground
+        if (!_isGrounded)
+        {
+            _downwardVelocity.y += gravity * Time.deltaTime;
+            //Falling Down
+            if (_downwardVelocity.y < 0)
+            {
+                _downwardVelocity.y += (jumpFallGravModifier - 1) * gravity * Time.deltaTime;
+            }
+        }
+
+        Debug.Log(_controller.velocity);
+
+        if (!_isGrounded && _hasJumped)
+        {
+            if ((int)Mathf.Sign(_downwardVelocity.y) != (int)Mathf.Sign(_lastDownwardVelocity.y))
+            {
+                _hasJumped = false;
+                if (_jumpTopTimer == 0)
+                {
+                    //if the y velocity of the player is less than 
+                    _jumpTopTimer = jumpTopDelay;
+                }
+            }
+        }
+
+        if (_isGrounded && !_hasJumped)
+        {
+            _downwardVelocity.y = 0;
+        }
+
+        if (_jumpTopTimer > 0)
+        {
+            _downwardVelocity.y = 0;
+        }
+
+        _lastDownwardVelocity = _downwardVelocity;
+
+        _controller.Move(_downwardVelocity * Time.deltaTime);
+        return;
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        //visualizing ground check
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundDistance);
+
+        //visualizing jump height
+        Gizmos.color = Color.green;
+        //find the distance to the ground
+        RaycastHit hit;
+        Physics.Raycast(groundCheck.position, -transform.up, out hit, jumpHeight, groundMask);
+        //if it is more than the jump height, clamp it to the feet
+        var distanceToGround = hit.distance;
+        if (distanceToGround >= jumpHeight) distanceToGround = jumpHeight;
+        Gizmos.DrawWireCube(
+            new Vector3(groundCheck.position.x, groundCheck.position.y + jumpHeight - distanceToGround,
+                groundCheck.position.z),
+            new Vector3(.5f, 0, .5f));
     }
 }
